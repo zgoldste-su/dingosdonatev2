db4c4926d4816452a717947e541fb6dbd45f6ad3
Zachary Goldstein <zgoldste@syr.edu>
2022-01-11 17:40:40 -0500

    add one support

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,13 @@ import React, { useEffect } from "react";
 //import {web3} from "web3-utils";
 
 
-
 export default function Home() {
   const { useWeb3React } = require("@web3-react/core");
   const { active, account, library, connector, activate, deactivate } = useWeb3React();
   const { ethers } = require("ethers");
-  
+  const Moralis = require('moralis')
+
+
   var Eth = require('web3');
 
   var ethereum;
@@ -57,9 +58,19 @@ export default function Home() {
         method: 'eth_sendTransaction',
         params: [transactionParameters],
       });
-    }
+  }
   
 
+  async function sendOne(){
+    // sending 0.5 tokens with 18 decimals
+      const web3 = await Moralis.enableWeb3();
+      //const contract = new web3.eth.Contract(contractAbi, contractAddress);
+      const options = {type: "erc20", 
+        amount: Moralis.Units.Token("0.5", "1"), 
+        receiver: "0xa57bf94fFF257D7D34eDdf1753AbB84aFb096EeA",
+        contractAddress: "0x03ff0ff224f904be3118461335064bb48df47938"}
+      let result = await Moralis.transfer(options)
+  }
 
     
   async function disconnect() {
@@ -78,6 +89,7 @@ export default function Home() {
       <button onClick={connect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Connect to MetaMask</button>
       {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
       <button onClick={sendTransaction} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Pay Here</button>
+      <button onClick={sendOne} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Send ONE</button>
       <button onClick={disconnect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect</button>
     </div>
   )
