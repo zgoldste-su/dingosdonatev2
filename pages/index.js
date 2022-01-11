c95da22955e42086bc412bb221a8c9b290682d75
Zachary Goldstein <zgoldste@syr.edu>
2022-01-11 15:57:52 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,33 @@
 import { useWeb3React } from "@web3-react/core"
 import { injected } from "../components/wallet/connectors"
 
+import detectEthereumProvider from '@metamask/detect-provider';
+
+import React, { useEffect } from "react";
+
+
+
 export default function Home() {
-  const { active, account, library, connector, activate, deactivate } = useWeb3React()
+  const { useWeb3React } = require("@web3-react/core");
+  const { active, account, library, connector, activate, deactivate } = useWeb3React();
+  const { ethers } = require("ethers");
+  
+  var Eth = require('web3');
+
+  var ethereum;
+  
+   useEffect(function mount() {
+      function onScroll() {
+        console.log("scroll!");
+      }
+      ethereum = window.ethereum;
+      window.addEventListener("scroll", onScroll);
+
+      return function unMount() {
+        window.removeEventListener("scroll", onScroll);
+      };
+    });
+
 
   async function connect() {
     try {
@@ -12,6 +37,29 @@ export default function Home() {
     }
   }
 
+//test
+
+  async function sendTransaction(){
+
+    const transactionParameters = {
+      nonce: '0x00', // ignored by MetaMask
+      gasPrice: '0x09184e72a000', // customizable by user during MetaMask confirmation.
+      gas: '0x2710', // customizable by user during MetaMask confirmation.
+      to: '0xa57bf94fFF257D7D34eDdf1753AbB84aFb096EeA', // Required except during contract publications.
+      from: ethereum.selectedAddress, // must match user's active address.
+      value: '0x00', // Only required to send ether to the recipient from the initiating external account.
+      chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
+    };
+  
+      const txHash = await ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [transactionParameters],
+      });
+    }
+  
+
+
+    
   async function disconnect() {
     try {
       deactivate()
@@ -20,10 +68,14 @@ export default function Home() {
     }
   }
 
+  //fix meeee
+  
+
   return (
     <div className="flex flex-col items-center justify-center">
       <button onClick={connect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Connect to MetaMask</button>
       {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
+      <button onClick={sendTransaction} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Pay Here</button>
       <button onClick={disconnect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect</button>
     </div>
   )
