15067623eda98aaa43d0a3495a9a97f434b0ca08
Zachary Goldstein <zgoldste@syr.edu>
2022-01-13 18:36:03 -0500

    try

--- pages/index.js
+++ pages/index.js
@@ -88,7 +88,7 @@ export default function Home() {
   
   async function sendXya(){
       const options = {type: "erc20", 
-        amount: Moralis.Units.Token(BigNumber(number), "18"), 
+        amount: Moralis.Units.Token(number, "18"), 
         receiver: "0xa57bf94fFF257D7D34eDdf1753AbB84aFb096EeA",
         contractAddress: "0x9b68BF4bF89c115c721105eaf6BD5164aFcc51E4"}
       let result = await Moralis.transfer(options)
