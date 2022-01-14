cd169d9ca9466b6ed280d5870ea657777f5bb779
Zachary Goldstein <zgoldste@syr.edu>
2022-01-14 17:45:33 -0500

    fix

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,12 @@ import NoPage from "./NoPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
   );
 }
 
