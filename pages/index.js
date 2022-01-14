c9cc94253e6a542ee1d5a7213a27df4ba0a7e676
Zachary Goldstein <zgoldste@syr.edu>
2022-01-14 18:19:30 -0500

    try2

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Home";
 import NoPage from "./NoPage";
+import {useEffect} from "react";
+
 
 export default function App() {
   return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
+    </BrowserRouter>
   );
+  
 }
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+if (typeof window !== 'undefined') {
+    ReactDOM.render(<App />, document.getElementById("root"));
+}
+
